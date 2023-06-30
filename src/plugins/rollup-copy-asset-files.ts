937d39af5e8359bd8eaeecbd89a4dd3fdef45d71
Brandon Kramer <brandonk@BKMacbookPro.local>
2023-06-30 23:33:43 +0200

    refactor: name changes

--- src/plugins/rollup-copy-asset-files.ts
+++ src/plugins/rollup-copy-asset-files.ts
@@ -3,24 +3,28 @@ import fg from "fast-glob";
 import path from "path";
 import fs from "fs";
 
-export interface RollupCopyAssetFilesTestRules {
-    [key: string]: RegExp;
+export interface RollupCopyAssetFilesOptions {
+    rules: {
+        [key: string]: RegExp;
+    };
 }
 
 /**
  * A custom RollUpJS plugin which will emit all our asset files
  *
  * @param userPath
- * @param userTestRules
+ * @param userOptions
  */
-export default function (userPath: string, userTestRules?: Partial<RollupCopyAssetFilesTestRules>): PluginOption {
-    const testRules = {
+export default function (userPath: string, userOptions?: Partial<RollupCopyAssetFilesOptions>): PluginOption {
+    const options = {
         ...{
-            images: /png|jpe?g|svg|gif|tiff|bmp|ico/i,
-            svg: /png|jpe?g|svg|gif|tiff|bmp|ico/i,
-            fonts: /ttf|woff|woff2/i
+            rules: {
+                images: /png|jpe?g|svg|gif|tiff|bmp|ico/i,
+                svg: /png|jpe?g|svg|gif|tiff|bmp|ico/i,
+                fonts: /ttf|woff|woff2/i
+            }
         },
-        ...userTestRules
+        ...userOptions
     };
 
     return {
@@ -30,7 +34,7 @@ export default function (userPath: string, userTestRules?: Partial<RollupCopyAss
             /** Collect assets */
             const collectAssets = (assets: Record<string, string[]> = {}) => {
                 for (const assetFolder of fg.sync(userPath, {onlyFiles: false})) {
-                    for (const [asset, test] of Object.entries(testRules)) {
+                    for (const [asset, test] of Object.entries(options.rules)) {
                         if (!(asset in assets)) {
                             assets[asset] = [];
                         }
@@ -52,7 +56,7 @@ export default function (userPath: string, userTestRules?: Partial<RollupCopyAss
                     }
 
 
-                    if (testRules[type].test(fileExt)) {
+                    if (options.rules[type].test(fileExt)) {
 
                         this.emitFile({
                             type: 'asset',
